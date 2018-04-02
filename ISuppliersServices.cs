c4f77c138303ea3567c266d7a7869961620fd284
maschataksoumee <37994773+maschataksoumee@users.noreply.github.com>
2018-04-02 11:23:56 +0530

    Implementation

---
+++ ISuppliersServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMS.Helpers;
+
+namespace IMS.Controllers.Services
+{
+    interface ISuppliersServices
+    {
+        Response CheckForDuplicateCode(Models.Stores.CheckForDuplicateCode.Request.DuplicateCodeCheckInput duplicateCodeCheckInput);
+    }
+}
